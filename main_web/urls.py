404da4114bb217ee61753fd852648ff1c66d39dd
tuguldur.kh <tuguldur.kh@corp.unitel.mn>
2026-01-15 18:41:40 +0800

    fixed major bug

--- main_web/urls.py
+++ main_web/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),       # Homepage
     path('contact/', views.contact, name='contact'), # Contact page
+    path('about/', views.about, name='about'),
+    path('history/', views.history, name='history'),
+    path('slogan/', views.slogan, name='slogan'),
+    path('social/', views.social, name='social'),
 ]
--- main_web/views.py
+++ main_web/views.py
@@ -5,3 +5,15 @@ def home(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def history(request):
+    return render(request, 'history.html')
+
+def slogan(request):
+    return render(request, 'slogan.html')
+
+def social(request):
+    return render(request, 'social.html')
