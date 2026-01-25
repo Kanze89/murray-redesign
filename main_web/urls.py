f8b203a1bf4931a0dbf4dae7e8a6c614b1b8d077
tuguldur.kh <tuguldur.kh@corp.unitel.mn>
2026-01-25 18:07:25 +0800

    Added executives URL and View

--- main_web/urls.py
+++ main_web/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
-# This list defines the URLs specifically for this app
 urlpatterns = [
-    path('', views.home, name='home'),       # Homepage
-    path('contact/', views.contact, name='contact'), # Contact page
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('history/', views.history, name='history'),
     path('slogan/', views.slogan, name='slogan'),
     path('social/', views.social, name='social'),
+    
+    # ADD THIS LINE:
     path('executives/', views.executives, name='executives'),
 ]
